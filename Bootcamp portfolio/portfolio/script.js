fcde81a14f77fc07782994422c69f5f98e6cd26a
SHAINKYPUNDIR <119501126+SHAINKYPUNDIR@users.noreply.github.com>
2023-04-07 15:13:25 +0530

    Add files via upload

---
+++ Bootcamp portfolio/portfolio/script.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".multiple-text",
+{
+    strings:["Frontend Developer","Youtuber","Blogger","Web Developer"],
+    typeSpeed:150,
+    backSpeed:150,
+    backDelay:100,
+    loop:true
+})
